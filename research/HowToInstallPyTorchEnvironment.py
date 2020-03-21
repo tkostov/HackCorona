dbf1812f1d18346dc3bc518feb0013a41253b78e
rheintzmann <heintzmann@gmail.com>
2020-03-21 19:48:38 +0100

    Added some how to install for the NN part and the ppx and pdf with the idea.

---
+++ research/HowToInstallPyTorchEnvironment.py
@@ -0,0 +1,13 @@
+# Notes on installation
+# conda create --name WirVsVirus python=3.6
+# conda activate WirVsVirus
+# conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
+# pip install pymongo
+# pip install sklearn
+# pip install matplotlib
+# pip install requests
+## Use Git to clone https://github.com/RainerHeintzmann/da-rnn.git
+# which is a fork of https://github.com/Seanny123/da-rnn
+# with bug fix in modules.py, according to https://github.com/Seanny123/da-rnn/issues/10
+
+# running the example takes about 10 min training on my Laptop GPU
