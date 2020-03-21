10d43379a6fd206c40e28313f44686aef8f6b713
Todor Kostov <todor.k.kostov@gmail.com>
2020-03-21 21:13:55 +0100

    Connected to backend

--- research/json_to_pandas.py
+++ research/json_to_pandas.py
@@ -3,6 +3,11 @@ import pandas as pd
 from joblib import delayed, Parallel
 import numpy as np
 from tqdm import tqdm
+from pymongo import MongoClient
+from bson.json_util import dumps as js_dumps
+from dotenv import load_dotenv
+import os
+
 
 # How to use -> view at bottom of file
 
@@ -15,20 +20,34 @@ from tqdm import tqdm
 
 class DataLoader(object):
     def __init__(self, from_back_end=False):
+        load_dotenv()
         if not from_back_end:
+            self.backend = False  #
             self.url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
         else:
-            self.url = ""  # TODO add BE url
+            self.backend = True
 
     def pull_data(self):
-        return requests.get(self.url).json()
+        if self.backend:
+            pwd = os.getenv("PWD_")
+            usr = os.getenv("USR_")
+            hostname = os.getenv("REMOTE_HOST")
+            client = MongoClient(
+                f"mongodb://{usr}:{pwd}@{hostname}:27017/wirvsvirus")
+            db = client["wirvsvirus"]
+            rki_collection = db["rkidata"]
+            return list(rki_collection.find())
+        else:
+            return requests.get(self.url).json()
 
     @staticmethod
     def parse_row(row_dict):
         # TODO we need to better parameterize this and then use the starmap to process the cols
         cols = ['IdBundesland', 'Bundesland', 'Landkreis', 'Altersgruppe', 'Geschlecht', 'AnzahlFall',
                 'AnzahlTodesfall', 'ObjectId', 'Meldedatum', 'IdLandkreis']
-        row_dict = row_dict["attributes"]
+
+        if "attributes" in row_dict:
+            row_dict = row_dict["attributes"]
         res = []
         for x in cols:
             if x in row_dict:
@@ -38,13 +57,14 @@ class DataLoader(object):
         return res
 
     def reshape_data(self, data_dict):
-        field_names = [x["name"] for x in data_dict["fields"]]
-        field_types = [x["sqlType"][7:] for x in data_dict["fields"]]
-        entries = data_dict["features"]
-        entries = Parallel(n_jobs=1)(delayed(DataLoader.parse_row)(x) for x in tqdm(entries))
-        dtype_mapping = {"Integer": "np.int64",
-                         "NVarchar": "str",
-                         "Other": "Object"}
+        field_names = ['IdBundesland', 'Bundesland', 'Landkreis', 'Altersgruppe', 'Geschlecht', 'AnzahlFall',
+                       'AnzahlTodesfall', 'ObjectId', 'Meldedatum', 'IdLandkreis']
+
+        if self.backend:
+            entries = data_dict
+        else:
+            entries = data_dict["features"]
+        entries = Parallel(n_jobs=32)(delayed(DataLoader.parse_row)(x) for x in tqdm(entries))
         rki_dataset = pd.DataFrame(entries, columns=field_names)
 
         data_dict_ = dict()
@@ -56,10 +76,11 @@ class DataLoader(object):
         data_dict = self.reshape_data(data_dict)
         return data_dict
 
+
 # Also as example
 
 if __name__ == "__main__":
-    dl = DataLoader() # instanciate DataLoader
-    data_dict = dl.process_data() # loads and forms the data dictionary
-    rki_data = data_dict["RKI_Data"] # only RKI dataframe
+    dl = DataLoader(from_back_end=True)  # instanciate DataLoader
+    data_dict = dl.process_data()  # loads and forms the data dictionary
+    rki_data = data_dict["RKI_Data"]  # only RKI dataframe
     print(rki_data.head())
