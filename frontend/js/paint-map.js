f90ae670e575486d8b9c2a9d46fa0e6adb5011af
Valentin <valentin.rublack@gmail.com>
2020-03-22 23:03:35 +0100

    Fixed slider == 0

--- frontend/js/paint-map.js
+++ frontend/js/paint-map.js
@@ -24,4 +24,4 @@
   var heat = L.heatLayer([]).addTo(map);
 })('map', 7);
 
-refreshMap(document.getElementById("rangeDays").value - 1, document.getElementById("socialDistancing").checked ? 1 : 0);
+refreshMap(document.getElementById("rangeDays").value, document.getElementById("socialDistancing").checked ? 1 : 0);
