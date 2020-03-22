0fec35313a01c93ea19da68af2fcc284331d55d2
Peter Majmesku <majmesku@publicplan.de>
2020-03-22 17:19:15 +0100

    Slider

--- frontend/js/show-from-api.js
+++ frontend/js/show-from-api.js
@@ -1,5 +1,8 @@
 // the API for Infections (no filtering for now!)
-var INFECTIONS = "http://ec2-3-122-224-7.eu-central-1.compute.amazonaws.com:8080/lk_infections";
+// var INFECTIONS = "http://ec2-3-122-224-7.eu-central-1.compute.amazonaws.com:8080/lk_infections";
+let INFECTIONS = 'http://ec2-3-122-224-7.eu-central-1.compute.amazonaws.com:8080/lk_infections?days=0&factor=1';
+// @TODO: work with the following url in the slide function, if the api providing the data.
+// let INFECTIONS_DAY_PARAM = 'http://ec2-3-122-224-7.eu-central-1.compute.amazonaws.com:8080/lk_infections?days=_DAY_&factor=1';
 
 function callAPI(method, page, params, onloadF) {
 	var xhr = new XMLHttpRequest();
@@ -38,6 +41,30 @@ function heatmapFunctionCases() {
 	var input = {};
 
 	callAPI('GET', INFECTIONS, input,
+		function () {
+			//log(this);
+			tt = this;
+			var response = JSON.parse(this.response);
+			rs = response;
+			for (var i = 0; i < response.length; i++) {
+				var p = response[i];
+				var g2d = p.geo_point_2d;
+				points2.push([g2d[0], g2d[1], p.AnzahlFall]);
+			}
+			showHeatmap(points2, getMax(points2));
+		});
+}
+
+function slide(day) {
+	points2 = [];
+	var input = {};
+
+	let INFECTIONS_DAY_PARAM;
+
+	// @TODO: work with the following url in the slide function, if the api providing the data.
+	// INFECTIONS_DAY_PARAM = INFECTIONS_DAY_PARAM.replace('_DAY_', day);
+	INFECTIONS_DAY_PARAM = INFECTIONS;
+	callAPI('GET', INFECTIONS_DAY_PARAM, input,
 		function () {
 			//log(this);
 			tt = this;
