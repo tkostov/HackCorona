4414aab565e870da6fef7ae225212487ec4b3bd8
ciureanuconstantin <62482554+ciureanuconstantin@users.noreply.github.com>
2020-03-22 12:04:26 +0100

    Use Infections API

---
+++ frontend/Constantin/coronaHeatmaps.js
@@ -0,0 +1,107 @@
+// the API for Infections (no filtering for now!)
+var INFECTIONS = "http://ec2-3-122-224-7.eu-central-1.compute.amazonaws.com:8080/lk_infections";
+
+function callAPI(method, page, params, onloadF) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, page, true);
+	xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.onloadFunction = onloadF; // save it for later!
+	
+	xhr.onload = function() {
+		 if (xhr.status != 200) { // analyze HTTP status of the response
+		} else { // show the result
+			xhr.onloadFunction();
+		}};
+	
+	logText("Calling: " + page);
+	xhr.send();
+}
+
+
+function getMax(p) {
+var max = 0;
+for(var i = 0; i < p.length; i++){
+   var v = p[i][2];
+   if (max < v) max = v;						
+}
+return max;
+}
+
+
+
+var points2 = [];
+var tt = null;
+var rs = null;
+
+// heatmapFunctionCases + heatmapFunctionDeads COULD be merged!!!
+function heatmapFunctionCases() {
+    points2 = [];
+	if (heatmap == null) {
+		var input = {};
+		
+		callAPI('GET', INFECTIONS, input, 
+		  function () {
+			//log(this);
+			  tt = this;
+			 var response = JSON.parse(this.response);
+			 rs = response;
+			for (var i = 0; i < response.length; i++) {
+				var p = response[i];
+				var g2d = p.geo_point_2d;
+				points2.push([g2d[0], g2d[1], p.AnzahlFall]);
+			}
+			showHeatmap(points2, getMax(points2));
+		   });
+	}
+	else {
+		showHeatmap([]); // wipes the old heatmap!
+	}
+}
+
+function heatmapFunctionDeads() {
+    points2 = [];
+	if (heatmap == null) {
+		var input = {};
+		
+		callAPI('GET', INFECTIONS, input, 
+		  function () {
+			//log(this);
+			  tt = this;
+			 var response = JSON.parse(this.response);
+			 rs = response;
+			for (var i = 0; i < response.length; i++) {
+				var p = response[i];
+				var g2d = p.geo_point_2d;
+				points2.push([g2d[0], g2d[1], p.AnzahlTodesfall]);
+			}
+			showHeatmap(points2, getMax(points2));
+		   });
+	}
+	else {
+		showHeatmap([]); // wipes the old heatmap!
+	}
+}
+
+
+
+var heatmap = null;
+var ratio = 200.0; // I don't like the ration / why is this value so large needed?
+
+function cleanHeatmap() {
+	heatmap.removeFrom(map);
+	heatmap = null;
+}
+
+function showHeatmap(heatdata, maxValue) {
+	if (heatmap == null) {
+		heatmap = L.heatLayer(heatdata, 
+		{radius: 50, max: maxValue / ratio,
+		blur: 50,
+		gradient: {0.0: 'blue', 0.5: 'lime', 1: 'red'}
+		}).addTo(map);
+	} else {
+		cleanHeatmap();
+	}
+	
+	return heatmap;
+}
