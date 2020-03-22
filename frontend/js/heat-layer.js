2dd897f978a589101cceb260e2900c18377dee13
Peter Majmesku <majmesku@publicplan.de>
2020-03-22 12:48:45 +0100

    temp

--- frontend/js/heat-layer.js
+++ frontend/js/heat-layer.js
@@ -2,13 +2,13 @@
 
 L.HeatLayer = (L.Layer ? L.Layer : L.Class).extend({
 
-    // options: {
-    //     minOpacity: 0.05,
-    //     maxZoom: 18,
-    //     radius: 25,
-    //     blur: 15,
-    //     max: 1.0
-    // },
+    options: {
+        minOpacity: 0.05,
+        maxZoom: 18,
+        radius: 50,
+        blur: 15,
+        max: 1.0
+    },
 
     initialize: function (latlngs, options) {
         this._latlngs = latlngs;
