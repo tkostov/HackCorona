d4880531cbbc3cf7df4a0874dc5d2944b5a45e25
agupta <anish-gupta@outlook.com>
2020-03-29 05:53:32 +0100

    wrapper reformats the data + creates an R object

--- R/wrapper.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-from rpy2.robjects import r, pandas2ri
---
+++ research/wrapper.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from rpy2.robjects import r, pandas2ri
+from json_to_pandas import DataLoader
+
+# Columns with translations to avoid headaches:
+
+# IdBundesland      | IdFederalstate
+# Bundesland        | Federalstate
+# Landkreis         | District
+# Altersgruppe      | Agegroup
+# Geschlecht        | Gender
+# AnzahlFall        | NumberofCases
+# AnzahlTodesfall   | NumberofDeaths
+# ObjectId          | ObjectId
+# Meldedatum        | Registrationdate
+# IdLandkreis       | IdDistrict
+# Bev Insgesamt     | Bev Overall
+
+# Relevant columns from RKI data and the corresponding columns of the ebola model
+# (index column) -> week
+#   might need incrementing by one
+#   (column name semantically incorrect)
+# Meldedatum -> date
+#   currently in unix time format (in milliseconds?!)
+#   needs to be mapped to R Dates
+# Bundesland -> country
+#   Could use Landkreis instead, ask Todor
+#   (column name semantically incorrect)
+# AnzahlFall -> cases
+#   Not sure if we need further processing, not 100% sure what cases represents in the R file
+#   Slightly suspicious because the column looks different (cases consists of decimals, AnzahlFall has small integers)
+
+# (stolen from json_to_pandas.py)
+dl = DataLoader()  # instantiate DataLoader
+data_dict = dl.process_data()  # loads and forms the data dictionary
+rki_data = data_dict["RKI_Data"]  # only RKI dataframe
+#print(rki_data.head())
+
+# formatted will contain the data adapted for the R code 
+# Extract relevant columns
+formatted = rki_data[["Meldedatum", "Bundesland", "AnzahlFall"]]
+
+# Add the week column
+formatted.insert(loc=0, column="week", value=(formatted.index+1))
+
+# Rename columns
+formatted.rename(columns={
+    "Meldedatum": "date",
+    "Bundesland": "country",
+    "AnzahlFall": "cases"
+}, inplace=True)
+
+# Convert from UNIX format to datetimes, might want to format further
+# TODO: check if this accurately converts, e.g. accounts for calendrical oddities
+formatted["date"] = pd.to_datetime(formatted["date"], unit="ms")
+
+# convert to R object
+pandas2ri.activate()
+dat = pandas2ri.py2ri(formatted)
+
+# Example:
+from rpy2.robjects.packages import importr
+utils = importr('utils')
+print(utils.head(dat))
