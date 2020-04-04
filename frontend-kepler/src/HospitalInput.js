f5db89236ff5fc1bc7cf5b3ef08fa75fd66bca6a
Patrick Ruoff <patrick.ruoff@inovex.de>
2020-04-04 21:30:31 +0200

    linechart test

--- frontend-kepler/src/HospitalInput.js
+++ frontend-kepler/src/HospitalInput.js
@@ -5,6 +5,12 @@ import CountrySelect from './CountrySelect'
 import {Link} from "react-router";
 import Button from "@material-ui/core/Button";
 
+
+import Title from './Title';
+import { useTheme } from '@material-ui/core/styles';
+import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
@@ -14,25 +20,89 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function HospitalInput() {
-    const classes = useStyles();
 
-    return (
-        <form className={classes.root} noValidate autoComplete="off">
-        <TextField required id="standard-basic" label="Hospital" />
-        <TextField required id="standard-basic" label="City" />
-        <CountrySelect/>
-        <TextField required id="standard-basic" label="ICUs in use" />
-        <TextField required id="standard-basic" label="ICU capacity" />
-        <Link to={"/"}><Button
-    type="submit"
-    fullWidth
-    variant="contained"
-    color="primary"
-    className={classes.submit}
-        >
-        Submit
-    </Button></Link>
-        </form>
-);
+// Generate Sales Data
+function createData(time, amount) {
+  return { time, amount };
 }
+
+const data = [
+  createData('00:00', 0),
+  createData('03:00', 300),
+  createData('06:00', 600),
+  createData('09:00', 800),
+  createData('12:00', 1500),
+  createData('15:00', 2000),
+  createData('18:00', 2400),
+  createData('21:00', 2400),
+  createData('24:00', undefined),
+];
+
+export default function HospitalInput() {
+  const theme = useTheme();
+
+  console.log(data)
+  return (
+    <React.Fragment>
+      <Title>Today</Title>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        >
+          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
+          <YAxis stroke={theme.palette.text.secondary}>
+            <Label
+              angle={270}
+              position="left"
+              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
+            >
+              Sales ($)
+            </Label>
+          </YAxis>
+          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </React.Fragment>
+  );
+}
+
+
+// export default function HospitalInput() {
+//     const classes = useStyles();
+//     const theme = useTheme();
+//
+//     return (
+//
+//
+//         <form className={classes.root} noValidate autoComplete="off">
+//         <TextField required id="standard-basic" label="Hospital" />
+//         <TextField required id="standard-basic" label="City" />
+//         <TextField required id="standard-basic" label="State" />
+//
+//         <CountrySelect/>
+//         <TextField required id="standard-basic" label="ICUs" />
+//         <TextField required id="standard-basic" label="ICU capacity" />
+//         <>
+//             <label>Required immediately</label>
+//         </>
+//
+//         <Link to={"/"}>
+//             <Button
+//                 type="submit"
+//                 fullWidth
+//                 variant="contained"
+//                 color="primary"
+//                 className={classes.submit}
+//                 >
+//             Submit
+//             </Button></Link>
+//         </form>
+//
+//   );
+// }
